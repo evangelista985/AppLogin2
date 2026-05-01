a3459b11c9078f995d15156b69b88c074ecc7e3d
Fabio Evangelista <evangelistafabio985@gmail.com>
2026-04-30 22:44:46 -0300

    atualizações Etec

--- app/src/main/java/com/example/applogintest/network/ApiClient.kt
+++ app/src/main/java/com/example/applogintest/network/ApiClient.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.net.URL
 
 object ApiClient {
-    private val URL = "http://192.168.10.120:8080/"
+    private val URL = "http://192.168.10.108:8080/"
     val instance: ApiService by lazy{
         val retrofit = Builder()
             .baseUrl(URL)
